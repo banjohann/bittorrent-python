27873dcf2fb995e6d9ee8e11834e379461096f75
Johann Bandelow <johannabandelow@gmail.com>
2025-05-29 19:58:14 -0300

    corrigido script de setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import random
 
 def generate_pieces(num_pieces=20, min_piece=1):
     max_piece = num_pieces * 5
-    pieces_dir = os.path.join(os.path.dirname(__file__), "pieces")
+    pieces_dir = os.path.join(os.path.dirname(__file__), "client/pieces")
     os.makedirs(pieces_dir, exist_ok=True)
     all_ids = list(range(min_piece, max_piece))
     selected_ids = random.sample(all_ids, num_pieces)
